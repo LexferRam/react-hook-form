311ce642b77093a96f093bfe869403f8403c996d
Lexfer <lexferramirez@gmail.com>
2021-11-13 22:46:31 -0400

    add date validation

--- src/FormYup.js
+++ src/FormYup.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+// import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
 import axios from 'axios'
-
+import moment from 'moment';
+import 'moment/locale/pt-br';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import { MenuItem } from '@mui/material';
+import { Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, MenuItem, Radio, RadioGroup } from '@mui/material';
 
 const schema = yup.object().shape({
     name: yup.string().required("Nombre es requerido!"),
@@ -20,22 +24,29 @@ const schema = yup.object().shape({
     confirmPassword: yup.string().oneOf([yup.ref("password"), null], 'Las contrasenas deben ser iguales'),
     optionSelect: yup.string().required("Debe seleccionar una opcion"),
     infoSelect: yup.string().required("Debe seleccionar una opcion"),
+    acceptTerms: yup.bool().oneOf([true], 'Accept Ts & Cs is required'),
+    radioOptions: yup.string().required('Favor responder el quiz').nullable(),
+    dateSelected: yup.date().required("Ingrese una fecha"),
+    dateSelected2: yup.date().min(
+        yup.ref('dateSelected'),
+        "Fecha de culminacion debe ser mayor a la de inicio"
+      )
 })
 
-const array = [
-    { id: 1, desc: 'todos' },
-]
 
 function FormYup() {
 
     const [contentInffo, setContentInfo] = useState([])
+    const [array, setArray] = useState([{ id: 1, desc: 'todos' }])
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
         resolver: yupResolver(schema),
     })
 
     const onSubmit = (data) => {
+        data.dateSelected = moment(data.dateSelected).format("DD/MM/YYYY");
         console.log(data)
+        reset();
     }
 
     const handleChangeCiudadSel = async (e) => {
@@ -65,6 +76,7 @@ function FormYup() {
                         <InputLabel>Confirm Password:</InputLabel>
                         <TextField margin="dense" error={Boolean(errors.confirmPassword)} helperText={errors.confirmPassword?.message} variant="outlined" {...register("confirmPassword")} type="password" placeholder="Confirm Password" />
 
+                        {/* SELECT LIST */}
                         <TextField
                             select
                             label="CIUDAD"
@@ -86,11 +98,11 @@ function FormYup() {
                         <TextField
                             select
                             label="Content Info."
-                            name="infoSelect"
                             margin='dense'
                             fullWidth
                             {...register('infoSelect')}
-                            error={Boolean(errors.infoSelect)} helperText={errors.infoSelect?.message}
+                            error={Boolean(errors.infoSelect)} 
+                            helperText={errors.infoSelect?.message}
                             disabled={contentInffo.toString() === '' ? true : false}
                             defaultValue=''
                         >
@@ -100,6 +112,81 @@ function FormYup() {
                                 </MenuItem>
                             ))}
                         </TextField>
+
+                        {/* CHECKBOX */}
+                        <FormControl
+                            required
+                            error={Boolean(errors.acceptTerms)}
+                            component="fieldset"
+                            sx={{ m: 3 }}
+                            variant="standard"
+                        >
+                            <FormControlLabel
+                                control={
+                                    <Checkbox {...register("acceptTerms")}  />
+                                }
+                                label="Terminos y condiciones"
+                            />
+                            <FormHelperText>{errors.acceptTerms?.message}</FormHelperText>
+                        </FormControl>
+
+                        {/* RADIO Button */}
+                        <FormControl
+                            sx={{ m: 3 }}
+                            component="fieldset"
+                            error={Boolean(errors.radioOptions)}
+                            variant="standard"
+                        >
+                            <FormLabel component="legend">Pop quiz: MUI is...</FormLabel>
+                            <RadioGroup
+                                aria-label="quiz"
+                            >
+                                <FormControlLabel value="best" control={<Radio defaultValue=''  {...register("radioOptions")} />} label="The best!" />
+                                <FormControlLabel value="worst" control={<Radio defaultValue='' {...register("radioOptions")} />} label="The worst." />
+                            </RadioGroup>
+                            <FormHelperText>{errors.radioOptions?.message}</FormHelperText>
+                        </FormControl>
+
+
+                        {/* DATE */}
+                        {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DesktopDatePicker
+                                label="Date desktop"
+                                inputFormat="MM/dd/yyyy"
+                                // value={value}
+                                renderInput={(params) => <TextField {...params} />}
+                                {...register("dateSelected")}
+                            />
+                        </LocalizationProvider> */}
+                        <TextField
+                            margin="normal"
+                            id="date"
+                            label="Birthday"
+                            {...register("dateSelected") }
+                            type="date"
+                            defaultValue={moment().format("YYYY-MM-DD")}
+                            sx={{ width: 220 }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            error={Boolean(errors.dateSelected)}
+                            helperText={errors.dateSelected?.message}
+                        />
+
+                        <TextField
+                            margin="normal"
+                            id="date2"
+                            label="Birthday2"
+                            {...register("dateSelected2")}
+                            type="date"
+                            defaultValue={moment().format("YYYY-MM-DD")}
+                            sx={{ width: 220 }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            error={Boolean(errors.dateSelected2)}
+                            helperText={errors.dateSelected2?.message}
+                        />
 
                         <Button type="submit" fullWidth >Enviar</Button>
                     </div>
